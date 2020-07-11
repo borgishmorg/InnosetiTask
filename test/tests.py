0bdc291e16d8e3f929c2860cca08644d8f7293e2
Aleksei Semenov <alexsemenov1610@gmail.com>
2020-07-11 17:50:36 +0200

    add new tests

--- test/tests.py
+++ test/tests.py
@@ -25,6 +25,26 @@ TESTS = [
         'value': 'Please specify a \'declared_price\''
     },
     {
+        'label': '2020-06-01 Glass 1000 test',
+        'json': {
+            'date': '2020-06-01',
+            'cargo_type': 'Glass',
+            'declared_price': '1000'
+        },
+        'key': 'price',
+        'value': '40'
+    },
+    {
+        'label': '2020-06-01 Other 1000 test',
+        'json': {
+            'date': '2020-06-01',
+            'cargo_type': 'Other',
+            'declared_price': '1000'
+        },
+        'key': 'price',
+        'value': '10'
+    },
+    {
         'label': '2020-07-01 Glass 1000 test',
         'json': {
             'date': '2020-07-01',
@@ -33,5 +53,25 @@ TESTS = [
         },
         'key': 'price',
         'value': '35'
+    },
+    {
+        'label': '2020-06-30 Other 1000 test',
+        'json': {
+            'date': '2020-06-30',
+            'cargo_type': 'Other',
+            'declared_price': '1000'
+        },
+        'key': 'price',
+        'value': '10'
+    },
+    {
+        'label': '2020-07-01 Glass 68923 test',
+        'json': {
+            'date': '2020-07-01',
+            'cargo_type': 'Glass',
+            'declared_price': '68923'
+        },
+        'key': 'price',
+        'value': '2413'
     }
 ]
